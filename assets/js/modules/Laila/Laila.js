895d25515560e9930fcac394089a17412a4785cf
lailaboris <liloboris05@gmail.com>
2024-10-03 13:31:36 +0200

    laila
    yes or no

--- assets/js/modules/Laila/Laila.js
+++ assets/js/modules/Laila/Laila.js
@@ -1,3 +1,34 @@
-export default function setupLaila() {
-  console.log("Hello from Laila");
+export default async function setupLaila() {
+  return fetch("https://yesno.wtf/api")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("nej, bare nej");
+      }
+      return response.json();
+    })
+
+    .then((data) => {
+      yesno(data);
+      return data;
+    })
+
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
 }
+
+function yesno() {
+  const yesnodisplay = document.getElementById("laila-p");
+  const yesnobutton = document.getElementById("laila-button");
+
+  yesnobutton.addEventListener("click", () => {
+    fetch("https://yesno.wtf/api") // replace with your API endpoint
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        
+      });
+  });
+}
+
